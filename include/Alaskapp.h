209b5c3d6a73b552297e6eae2e5aac79c7c1611e
brunno <brunnomurilo1@gmail.com>
2026-05-30 09:06:17 -0300

    movendo td pra classe jogo

---
+++ include/Alaskapp.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "pch.h"
+#include "Graficos.h"
+#include "Personagem.h"
+#include "Eventos.h"
+#include "Jogador.h"
+#include "Fase.h"
+#include "FaseNoite.h"
+
+namespace Alaska
+{
+    class Alaskapp
+    {
+        private:
+            Alaska::Gerenciadores::Graficos GG;
+            Alaska::Entidades::Personagens::Jogador* pJog1;
+        public:
+            Alaskapp();
+            ~Alaskapp();
+			void executar();
+    };
+}
