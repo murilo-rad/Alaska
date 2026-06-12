57a4e114c2299a6c5f8aacb7d7f6c32a0113e641
brunnomsants <brunnomurilo1@gmail.com>
2026-06-11 21:26:48 -0300

    set ponteiros Abominavel e Bola_de_Neve

--- src/Entidades/Bola_de_Neve.cpp
+++ src/Entidades/Bola_de_Neve.cpp
@@ -1,2 +1,6 @@
 #include "Bola_de_Neve.h"
-
+//#include "Abominavel.h"
+//
+//void Alaska::Entidades::Bola_de_Neve::setPAbm(Alaska::Entidades::Personagens::Abominavel* pAb) {
+//	pAbm = pAb;
+//}
--- src/Entidades/Personagens/Abominavel.cpp
+++ src/Entidades/Personagens/Abominavel.cpp
@@ -1,1 +1,6 @@
 #include "Abominavel.h"
+//#include "Bola_de_Neve.h"
+//
+//void Alaska::Entidades::Personagens::Abominavel::setPBola(Alaska::Entidades::Bola_de_Neve* pProj) {
+//	pBola = pProj;
+//}
